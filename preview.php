d6db4b41ce0fde5f0eb94c976d55651b9c04ac82
mragenks <theqar07@gmail.com>
2024-04-23 13:18:17 +0700

    preview

--- preview.php
+++ preview.php
@@ -8,9 +8,4 @@ include('sidebar.php');
     <source src="movie.ogg" type="video/ogg">
     Your browser does not support the video tag.
   </video>
-  <div class="controls">
-    <button class="volume-up">Volume Naik</button>
-    <input type="range" class="volume-slider" min="0" max="1" step="0.01" value="0.5">
-    <button class="volume-down">Volume Turun</button>
-  </div>
 </div>
