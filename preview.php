4ce2c196a842779a0ac2eee7c0d6284e63ffd7b0
mragenks <theqar07@gmail.com>
2024-04-23 13:00:55 +0700

    add video preview

--- preview.php
+++ preview.php
@@ -4,7 +4,7 @@ include('sidebar.php');
 <link rel="stylesheet" href="preview.css">
 <div class="video-player">
   <video width="640" height="360" controls>
-    <source src="movie.mp4" type="video/mp4">
+    <source src="video/Deadpool & Wolverine _ Trailer.mp4" type="video/mp4">
     <source src="movie.ogg" type="video/ogg">
     Your browser does not support the video tag.
   </video>
