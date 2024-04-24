9aed5cdab65ad539bf7529e34565572d2e817fa9
unknown <michdidimus@gmail.com>
2024-04-24 22:05:13 +0700

    msmsms

--- course.php
+++ course.php
@@ -1,11 +1,10 @@
 <?php
-require('video.php');
-
-// Membuat objek dari kelas Video
-$videoObj = new Video();
-
-// Mengambil data video dari database
-$videos = $videoObj->getVideoData();
+// Array berisi informasi tentang gambar dan judul video
+$videosData = array(
+    array("image" => "img/ui.jpeg", "title" => "Learn Figma: UI/UX Design Essential Training"),
+    array("image" => "img/Code.jpeg", "title" => "Python For Beginners - Learn Programming"),
+    array("image" => "img/Gitar.jpeg", "title" => "Acoustic Guitar And Electric Guitar Started")
+);
 ?>
 
 <?php include('sidebar.php'); ?>
@@ -13,9 +12,9 @@ $videos = $videoObj->getVideoData();
 <div class="top-courses">
   <h2>TOP COURSES</h2>
   <div class="course-grid">
-    <?php foreach($videos as $key => $video): ?>
+    <?php foreach($videosData as $key => $video): ?>
     <div class="course-item">
-      <img src="img/<?php echo $video['image']; ?>" alt="<?php echo $video['title']; ?>">
+      <img src="<?php echo $video['image']; ?>" alt="<?php echo $video['title']; ?>">
       <h3><?php echo $video['title']; ?></h3>
       <button>Start Course</button>
       <a href="preview.php?video_id=<?php echo $key; ?>"><button>Preview</button></a>
