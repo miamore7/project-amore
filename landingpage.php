d3ef3859b298b5eb68a13537de7a8424fbc2e09f
unknown <michdidimus@gmail.com>
2024-04-25 15:06:36 +0700

    s

---
+++ landingpage.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Find Your Right Mentor</title>
+	<link rel="stylesheet" type="text/css" href="landingpage.css">
+</head>
+<body>
+	<div class="container">
+		<div class="text-content">
+			<h1>Find your right mentor</h1>
+			<p>Learn from experts <br> Grow your hobby</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer.</p>
+			<button>Get Started</button>
+			<div class="stats">
+				<div class="stat">
+					<p>34+</p>
+					<p>Lorem ipsum dolor sit consectetur adipiscing</p>
+				</div>
+				<div class="stat">
+					<p>245K</p>
+					<p>Lorem ipsum dolor sit consectetur adipiscing</p>
+				</div>
+				<div class="stat">
+					<p>15+</p>
+					<p>Lorem ipsum dolor sit consectetur adipiscing</p>
+				</div>
+				<div class="stat">
+					<p>4.89</p>
+					<p>Lorem ipsum dolor sit consectetur adipiscing</p>
+				</div>
+			</div>
+		</div>
+		<div class="images">
+			<img src="img\Person1.jpg" alt="Person 1">
+			<img src="img\Person2.jpg" alt="Person 2">
+			<img src="img\Person3.jpg" alt="Person 3">
+			<img src="img\Person4.jpg" alt="Person 4">
+			<div style="display: flex; width: 100%;">
+				<img src="img\Person2.jpg" alt="Person 5" style="width: 50%;">
+				<img src="img\Person3.jpg" alt="Person 6" style="width: 50%;">
+			</div>
+		</div>
+	</div>
+	<script>
+
+	</script>
+</body>
+</html>
