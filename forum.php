553e9dabe9435f46b09d7f155be6b0dbabf89df8
unknown <michdidimus@gmail.com>
2024-05-14 17:23:11 +0700

    .

---
+++ forum.php
@@ -0,0 +1,42 @@
+<?php include('sidebar.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chat Example</title>
+  <link rel="stylesheet" href="css/forum.css">
+</head>
+<body>
+  <div class="container">
+    <div class="chat-container">
+      <div class="message">
+        <div class="message-content">
+          UXR/UX: You can only bring one item to a remote island to assist your research of native use of tools and usability. What do you bring? #TellMeAboutYou
+        </div>
+      </div>
+      <div class="message">
+        <div class="message-content">
+          Y'all ready for this next post?
+        </div>
+      </div>
+      <div class="message">
+        <div class="message-content">
+          Kobe's passing is really sticking w/ me in a way I didn't expect. <br><br>
+          He was an icon, the kind of person who wouldn't die this way. My wife compared it to Princess Di's accident. <br><br>
+          But the end can happen for anyone at any time, & I can't help but think of anything else lately.
+        </div>
+      </div>
+      <div class="message">
+        <div class="message-content">
+          UXR/UX: You can only bring one item to a remote island to assist your research of native use of tools and usability. What do you bring? #TellMeAboutYou
+        </div>
+      </div>
+      <div class="input-container">
+        <input type="text" placeholder="Type your message...">
+        <button>Send</button>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
